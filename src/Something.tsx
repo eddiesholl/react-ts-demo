16cda02643d6f4b73c76860f92dd7d19106a993f
Eddie Sholl <esholl@zendesk.com>
2019-03-10 21:25:31 +1100

    Convert Somethng component

--- src/Something.tsx
+++ src/Something.tsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import withProp from './withProp'
 
-export class Something extends Component {
+export interface SomethingProps {
+  prop1: string,
+  prop2: string,
+  extraFuncProp: (x: string) => string,
+  fromState: string
+}
+
+export class Something extends Component<SomethingProps, {}> {
   render() {
     const { prop1, prop2, extraFuncProp, fromState } = this.props
     
@@ -18,11 +24,5 @@ export class Something extends Component {
   }
 }
 
-Something.propTypes = {
-  prop1: PropTypes.string.isRequired,
-  prop2: PropTypes.string.isRequired,
-  extraFuncProp: PropTypes.func.isRequired,
-  fromState: PropTypes.string.isRequired
-}
 
 export default withProp(Something)
